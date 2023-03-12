46a5956f6b0e038353df692cfee9b22d6137c122
mateo <mateoslb70@gmail.com>
2023-03-12 14:37:00 -0500

    Added whitenoise and run collectstatic

--- ballot_store/settings.py
+++ ballot_store/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0!!kblwag^i$z&%@a#3u5zkz59582t^e-1$44003)0r%w3*mns
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'web-production-aea2.up.railway.app']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
