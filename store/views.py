0e1b5e7a0d0170e7daa3432f20a830418eb4c2ad
mateo <mateoslb70@gmail.com>
2023-03-13 17:02:01 -0500

    Changed extra key names in payload

--- store/views.py
+++ store/views.py
@@ -325,11 +325,11 @@ def fetch_api(request):
               "typeSell": "2", # 1 for email payment, 2 for via link, 3 via mobile SMS, 4 via social networks
               "tax": "0", 
               "email": client.correo,
-              "x_extra1": transaction.id,
-              "x_extra2": ", ".join([str(ballot.id) for ballot in ballots]),
+              "extra1": transaction.id,
+              "extra2": ", ".join([str(ballot.id) for ballot in ballots]),
               "urlConfirmation": "https://web-production-aea2.up.railway.app/epayco_confirmation",
               "urlResponse": "https://web-production-aea2.up.railway.app/epayco_response/{}/".format(transaction.id), 
-              "methodConfirmation": "GET",
+              "methodConfirmation": "GET", # request.method = 'POST' anyway
             #   "urlConfirmation": "http://127.0.0.1:8000/epayco_confirmation",
             #   "urlResponse": "http://127.0.0.1:8000/epayco_response/{}/".format(transaction.id),
               "expirationDate": timezone.localtime(transaction.valid_until).strftime('%Y-%m-%d %H:%M:%S')    # Format Date Time UTC payment link expiration date 
